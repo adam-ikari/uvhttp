459c4d7abd588562dc9e405048b20542ee2af5f3
Adam <chenzd123456@gmail.com>
2026-01-19 08:37:43 +0800

    chore: 更新版本号至 1.5.0
    - 更新 UVHTTP_VERSION_* 宏定义
    - 与最新发布版本保持一致

--- include/uvhttp.h
+++ include/uvhttp.h
@@ -42,9 +42,9 @@ extern "C" {
 
 /* 版本信息 */
 #define UVHTTP_VERSION_MAJOR 1
-#define UVHTTP_VERSION_MINOR 4
+#define UVHTTP_VERSION_MINOR 5
 #define UVHTTP_VERSION_PATCH 0
-#define UVHTTP_VERSION_STRING "1.4.0"
+#define UVHTTP_VERSION_STRING "1.5.0"
 
 /* API使用说明：
  * 
